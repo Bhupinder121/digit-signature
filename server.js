68d49c92757022a153405670168cf7ae3e1a8248
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2024-01-06 18:03:17 +0530

    did nothing

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const crypto = require('crypto')
+const { MongoClient } = require("mongodb");
 const app = express();
+
+const uri =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1";
+const client = MongoClient(uri);
+
+client.connect().then((res)=>{console.log("connected")});
+
 app.listen(3000, () => console.log("Server started on port 3000"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
@@ -8,14 +16,17 @@ app.get("/", (req, res) => {
   res.render("./public/index.html");
 });
 
+
+
 app.post("/generateKeyPair", (req, res) => {
-  const { userId } = req.body;
+  const { userId, message } = req.body;
   console.log(req.body);
   const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
   });
-  console.log(userId, publicKey, privateKey);
-  res.status(200).send("Ok");
+  crypto.crea
+  console.log(crypto.createHash("SHA256", "key").update(message).digest("hex"))
+  res.status(200).send({publicKey, privateKey});
   // Save the keys associated with the user ID...
 });
 
